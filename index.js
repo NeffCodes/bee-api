92fcd949d529a80106e73a823c6ae6121dc999f6
James Neff <james.m.neff@gmail.com>
2022-06-06 15:24:13 -0400

    get basic server started

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8000;
+
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/index.html')
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log('Displaying hive info to port:', PORT)
+})
