8a4eca43d3733553e24033ee65b655e7fb3fe2b9
James Neff <james.m.neff@gmail.com>
2022-06-06 15:58:49 -0400

    Basic API setup

--- index.js
+++ index.js
@@ -2,10 +2,38 @@ const express = require('express');
 const app = express();
 
 const PORT = 8000;
+const honeyBeeSpecies = {
+  'western' : {
+    name: "Western Honey Bee",
+    latin: 'apis Mellifera',
+    description: '',
+  },
+  'dark' : {
+    name: "Dark Honey Bee",
+    latin: 'apis Mellifera Mellifera',
+    description: '',
+  },
+  'unknown' : {
+    error: "bee not found"
+  },
+}
 
 app.get('/', (request, response) => {
   response.sendFile(__dirname + '/index.html')
 });
+
+app.get('/api', (request,response) => {
+  const removeUnknown = Object.entries(honeyBeeSpecies)
+   .filter( ([key,value]) => key !== 'unknown')
+  response.json( Object.fromEntries(removeUnknown) )
+})
+
+app.get('/api/:species', (request, response) => {
+  const species = request.params.species.toLowerCase();
+  honeyBeeSpecies[species] ?
+    response.json(honeyBeeSpecies[species]) :
+    response.json(honeyBeeSpecies["unknown"]);
+})
 
 app.listen(process.env.PORT || PORT, () => {
   console.log('Displaying hive info to port:', PORT)
